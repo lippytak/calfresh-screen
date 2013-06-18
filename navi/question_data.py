6300a5b08cce605396869487fcd361135b63cd8a
Jake Solomon <jacob@codeforamerica.org>
2013-06-18 16:54:49 -0700

    add question numbers

--- navi/question_data.py
+++ navi/question_data.py
@@ -1,37 +1,37 @@
 question_data = [
     {
       "key": "house_size",
-      "question_text": "How many people live in your HOUSEHOLD? Include everyone who shares income and expenses, like bills and food.",
-      "clarification_text": "How many people live in your household? Include everyone who shares income and expenses, like bills and food. Text a NUMBER, like 5.",
+      "question_text": "1. How many people live in your HOUSEHOLD? Include everyone who shares income and expenses, like bills and food.",
+      "clarification_text": "1. How many people live in your household? Include everyone who shares income and expenses, like bills and food. Text a NUMBER, like 5.",
       "type":"rangequestion"
     },
     {
       "key": "annual_income",
-      "question_text": "What is the overall ANNUAL INCOME of EVERYONE in your household? Include ALL the money you get.",
-      "clarification_text": "What is the overall ANNUAL INCOME of EVERYONE in your household? If you don't know, just take your best guess. Text a NUMBER, like 500.",
+      "question_text": "2. What is the overall ANNUAL INCOME of EVERYONE in your household? Include ALL the money you get.",
+      "clarification_text": "2. What is the overall ANNUAL INCOME of EVERYONE in your household? If you don't know, just take your best guess. Text a NUMBER, like 500.",
       "type":"rangequestion"
     },
     {
       "key": "pregnant_or_baby",
-      "question_text": "Are you PREGNANT or do you have any KIDS under 5 years old?",
-      "clarification_text": "Are you PREGNANT right now OR do you have any KIDS under 5 years old? Text YES or NO.",
+      "question_text": "3. Are you PREGNANT or do you have any KIDS under 5 years old?",
+      "clarification_text": "3. Are you PREGNANT right now OR do you have any KIDS under 5 years old? Text YES or NO.",
       "type":"yesnoquestion"
     },
     {
       "key": "kid_school",
-      "question_text": "Do you have any kids who go to SF PUBLIC SCHOOLS?",
-      "clarification_text": "Do you have any kids who are currently enrolled in a SF PUBLIC SCHOOLS? Text YES or NO.",
+      "question_text": "4. Do you have any kids who go to SF PUBLIC SCHOOLS?",
+      "clarification_text": "4. Do you have any kids who are currently enrolled in a SF PUBLIC SCHOOLS? Text YES or NO.",
       "type":"yesnoquestion"
     },
     {
       "key": "health_insurance",
-      "question_text": "Do you have HEALTH INSURANCE right now?",
-      "clarification_text": "Do you have HEALTH INSURANCE right now? This might be through Kaiser, Blue Cross, Medi-Cal, Healthy SF, or other insurance companies. Text YES or NO.",
+      "question_text": "5. Do you have HEALTH INSURANCE right now?",
+      "clarification_text": "5. Do you have HEALTH INSURANCE right now? This might be through Kaiser, Blue Cross, Medi-Cal, Healthy SF, or other insurance companies. Text YES or NO.",
       "type":"yesnoquestion"
     },
     {
       "key": "help",
-      "question_text": "Do you need help with anything? I can't reply but I promise to read it.",
+      "question_text": "6. Do you need help with anything? I can't reply but I promise to read it.",
       "clarification_text": "",
       "type":"freeresponsequestion"
     }
